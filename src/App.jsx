6b1e6aa55d8d4614dec00d8ba66519436f2d4af5
Shanmukh459 <saikrrish459@gmail.com>
2023-11-15 19:10:23 -0800

    Corrected Progress comp import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Progress from "./components/progress"
+import Progress from "./components/Progress"
 import { YoutubeTranscript } from 'youtube-transcript'
 import './App.css'
 
